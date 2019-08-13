3193c100f3ce9a2749af14a20a9cf0c02cfe4c1e
Yuliia_Serhiivna <juli.bondarenko1806@gmail.com>
2019-08-13 20:43:42 +0300

    added Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+num = 1123
+num_str = str(num)
+#multiplication num
+print(int(num_str[0]) * int(num_str[1]) * int(num_str[2]) * int(num_str[3]))
+#reverse num
+print(num_str [::-1])
+#sorted num
+print(sorted(num_str))
+ 
